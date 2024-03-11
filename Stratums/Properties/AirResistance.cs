861251c16fa1df426a4e88c0f625985376eedd54
Cruz Morgan <cruphobia@gmail.com>
2024-03-11 16:43:32 -0700

    Before creating new Entity Partitioning System

---
+++ Stratums/Properties/AirResistance.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Stratums.Entities;
+using Stratums.HelperMethods;
+using Stratums.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stratums.Properties
+{
+    public class AirResistance : Property
+    {
+        private float _decayRate;
+
+        public AirResistance() 
+        {
+            _decayRate = 0.997f;
+        }
+
+        public AirResistance(float decayRate) 
+        {
+            _decayRate = decayRate;
+        }
+
+        public override IEnumerable<RenderData> GetRenderData()
+        {
+            return Array.Empty<RenderData>();
+        }
+
+        public override void OnUpdate(GameTime deltaTime, EntityBatch entityBatch, ref EntityData entityData)
+        {
+            entityData.Velocity *= _decayRate;
+        }
+    }
+}
